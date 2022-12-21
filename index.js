3165bd38bba6a21a6e6d55ef6641716050d4fc68
Saylendra Yasin <saylendrayasin@gmail.com>
2022-12-21 22:02:29 +0800

    Syling navbar and add table to contact page

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const PORT = 3000;
-const controllers = require("./src/controllers/controller");
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  // res.send("Hello");
-  // res.sendFile("./src/pages/index.html", { root: __dirname });
   const mahasiswa = [
     {
       nama: "Saylendra",
@@ -27,41 +24,25 @@ app.get("/", (req, res) => {
   ];
   res.render("index", {
     nama: "Saylendra Yasin",
-    title: "Halaman Home",
+    title: "Homepage",
     mahasiswa: mahasiswa,
     layout: "layouts/main-layout.ejs",
   });
 });
 
 app.get("/about", (req, res) => {
-  // res.send("Hello, ini halaman about");
-  // res.sendFile("./src/pages/about.html", { root: __dirname });
   res.render("about", {
     layout: "layouts/main-layout.ejs",
-    title: "Halaman About",
+    title: "Aboutpage",
   });
 });
 
 app.get("/contact", (req, res) => {
-  // res.sendFile("./src/pages/contact.html", { root: __dirname });
   res.render("contact", {
     layout: "layouts/main-layout.ejs",
-    title: "Halaman Contact",
+    title: "Contactpage",
   });
 });
-
-app.get("/products/:id", (req, res) => {
-  res.send("Product ID : " + req.params.id);
-});
-
-app.get("/produk/:id", (req, res) => {
-  //cth localhost:3000/produk/12?kategori=sepatu
-  res.send(
-    `ID Produk : ${req.params.id} <br/> Kategori : ${req.query.kategori}`
-  );
-});
-
-app.get("/calculate/:a/:b", controllers.calculate);
 
 app.use("/", (req, res) => {
   res.status(404);
